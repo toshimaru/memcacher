45deed4b1e3907019f452c6fc470c65beeff52fd
toshimaru <me@toshimaru.net>
2017-05-10 11:21:59 +0900

    Add load config method

--- lib/active_support/cache/memcacher.rb
+++ lib/active_support/cache/memcacher.rb
@@ -5,6 +5,20 @@ module ActiveSupport
         puts "memcacher loaded!"
         super
       end
+
+      private
+
+        def memcache_config
+          config_path = "config/memcache.yml"
+          yaml = Pathname.new(config_path)
+
+          if yaml.exist?
+            require "yaml"
+            YAML.load(yaml.read)
+          else
+            raise "Could not load memcache configuration. No such file - #{config_path}"
+          end
+        end
     end
   end
 end
